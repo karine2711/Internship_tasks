0069b999475694ed29b58645354a738406a305a6
Karine <karine.gevorgyan.2001@mail.ru>
2020-03-09 22:09:45 +0400

    Completed task 2

---
+++ src/java8/task2/Main.java
@@ -0,0 +1,27 @@
+package java8.task2;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons =initPersons();
+        Comparator<Person> comparatorByAge = (person1, person2) -> person1.getAge() - person2.getAge();
+        Comparator<Person> comparatorByName = (person1, person2) -> person1.getName().compareTo(person2.getName());
+        persons.sort(comparatorByAge.thenComparing(comparatorByName));
+        printNames(persons);
+
+    }
+
+    static List <Person>initPersons(){
+        Person Karine=new Person("Karine", 19);
+        Person Ani =new Person("Ani",15);
+        Person Mukuch=new Person("Mukuch", 21);
+      return Arrays.asList(Karine,Ani,Mukuch);
+    }
+
+    static void printNames(List<Person> persons){
+        persons.stream()
+                .map(Person::getName)
+                .forEach(System.out::println);
+    }
+}

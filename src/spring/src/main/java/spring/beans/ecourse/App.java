0e665928c7ad56edd523501750bd278bf36987ea
Karine <karine.gevorgyan.2001@mail.ru>
2020-03-20 01:19:59 +0400

    Spring Ecourse- Lesson2,3

--- src/spring/src/main/java/spring/beans/ecourse/App.java
+++ src/spring/src/main/java/spring/beans/ecourse/App.java
@@ -1,18 +1,27 @@
 package spring.beans.ecourse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     Client client;
     ConsoleEventLogger eventLogger;
 
     public static void main(String[] args) {
-        App app = new App();
-        app.client = new Client(1,"John Smith");
-        app.eventLogger=new ConsoleEventLogger();
-        app.logEvent("Some event for user1");
+        ApplicationContext context=new ClassPathXmlApplicationContext("ecourse.xml");
+        App app=context.getBean("app", App.class);
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
+
     }
 
     private void logEvent(String msg){
         String message=msg.replaceAll(Integer.toString(client.getId()), client.getFullName());
         eventLogger.logEvent(message);
+    }
+
+    public App(Client client, ConsoleEventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
     }
 }
